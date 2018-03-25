58329c0ebdaa85f93136729d8c51d1f4e9a6cf87
Mitchell <nowheredust@gmail.com>
2018-03-24 20:19:24 -0400

    Tests to allow a user to input their name and have it returned in a bix below. More of a refresher than an important part of the project

--- app/src/main/java/com/example/mitchell/newprojectthree/MainActivity.java
+++ app/src/main/java/com/example/mitchell/newprojectthree/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.mitchell.newprojectthree;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //You sugar gay
+    }
+
+    public void buttonClickTest(View v){
+        TextView newNameDisplay = findViewById(R.id.newNameDisplay);
+        EditText nameText = findViewById(R.id.nameInput);
+        Editable name = nameText.getText();
+        newNameDisplay.setText(name);
     }
 }
